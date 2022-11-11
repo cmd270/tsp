d77e3a0862ddebcbcd16294399a9432cd2a2f5e7
cmd270 <cmd345zxc@gmail.com>
2022-11-11 18:26:24 +0800

    v0.1

---
+++ CMakeFiles/salesman.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for salesman.
+# This may be replaced when dependencies are built.
--- salesman.cpp
+++ salesman.cpp
@@ -4,29 +4,29 @@
 #include <ctime>
 #include <iostream>
 
-int** generateGraph(int const size);
+int** generateGraph(int const *size);
 void showGraph(int** const graph,int const *size);
 
 int main(){
     int size = 5;
-    int **graph = generateGraph(size);
+    int **graph = generateGraph(&size);
     showGraph(graph,&size);
    return 0;
 }
 
-int** generateGraph(int const size){
-   int **graph = new int* [2];
-   graph[0] = new int[size];
-   graph[1] = new int[size];
+int** generateGraph(int const *size){
+   int **graph = new int* [*size];
+   for(int i = 0; i < *size; ++i){
+       graph[i]=new int[*size];
+   }
    std::srand(std::time(NULL));
-   ptrdiff_t i,j;
-   for(i = 0; i < size; ++i){
-       for(j = 0; j < size;++i){
+   for(int i = 0; i < *size; ++i){
+       for(int j = 0; j < *size;++j){
            if(i == j){
               graph[i][j] = 0;
            }
            else{
-               graph[i][j] = std::rand() % ( 100 - 1 +1);
+               graph[i][j] = std::rand() % 30;
                graph[j][i] = graph[i][j];
            }
        }
